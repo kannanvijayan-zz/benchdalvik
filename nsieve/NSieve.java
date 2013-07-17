077782a57cdead1b681fb5b8b5e05bb6de18c981
Kannan Vijayan <kannan.vij@gmail.com>
2013-07-17 17:56:40 -0400

    Added nsieve code.

---
+++ nsieve/NSieve.java
@@ -0,0 +1,46 @@
+// The Great Computer Language Shootout
+// http://shootout.alioth.debian.org/
+//
+// modified by Isaac Gouy
+// ported to Java by Kannan Vijayan
+
+import java.util.Date;
+
+public class NSieve
+{
+    public static int nsieve(int m, boolean[] isPrime) {
+        int i, k, count;
+
+        for (i = 2; i <= m; i++) { isPrime[i] = true; }
+        count = 0;
+
+        for (i = 2; i <= m; i++) {
+            if (isPrime[i]) {
+                for (k = i + i; k <= m; k += i)
+                    isPrime[k] = false;
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int sieve() {
+        int count = 0;
+        for (int i = 1; i <= 3; i++) {
+            int m = (1 << i) * 10000;
+            boolean[] flags = new boolean[m + 1];
+            count += nsieve(m, flags);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Date d1 = new Date();
+        int count = 0;
+        for (int i = 0; i < 100; i++)
+            count += sieve();
+        Date d2 = new Date();
+        System.out.println("Java Time: " + ((d2.getTime() - d1.getTime()) / 1000.0) +
+                           " (count=" + count + ")");
+    }
+}
---
+++ nsieve/nsieve.c
@@ -0,0 +1,54 @@
+// The Great Computer Language Shootout
+// http://shootout.alioth.debian.org/
+//
+// modified by Isaac Gouy
+// ported to C by Kannan Vijayan
+
+#include <sys/time.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+double DoubleTime() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (double)tv.tv_sec + (((double)tv.tv_usec) / 1000000);
+}
+
+int nsieve(int m, bool *isPrime) {
+    int i, k, count;
+
+    for (i = 2; i <= m; i++) { isPrime[i] = true; }
+    count = 0;
+
+    for (i = 2; i <= m; i++) {
+        if (isPrime[i]) {
+            for (k = i + i; k <= m; k += i)
+                isPrime[k] = false;
+            count++;
+        }
+    }
+    return count;
+}
+
+int sieve() {
+    int count = 0;
+    for (int i = 1; i <= 3; i++) {
+        int m = (1 << i) * 10000;
+        bool *flags = (bool*) malloc(sizeof(bool) * (m + 1));
+        count += nsieve(m, flags);
+        free(flags);
+    }
+    return count;
+}
+
+int main() {
+    int count = 0;
+    double d1 = DoubleTime();
+    for (int i = 0; i < 100; i++) {
+        count += sieve();
+    }
+    double d2 = DoubleTime();
+    fprintf(stderr, "CXX Time: %lf  (count=%d)\n", (d2 - d1), count);
+    return 0;
+}
